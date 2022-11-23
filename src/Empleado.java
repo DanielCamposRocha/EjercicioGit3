45574c27c167228fe2774ea8f85514cef556f0e0
a22danielcr <danielcamposrocha9@gmail.com>
2022-11-23 19:46:40 +0100

    Actualizo el readme mas legible, modifico la para separar la impresion

---
+++ src/Empleado.java
@@ -0,0 +1,14 @@
+public class Empleado {
+    private String nombre; // Ahora el campo nombre es inmutable.
+    int edad;
+
+    Empleado(String name, int edad) {
+        this.nombre = name;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "El empleado " + nombre + " tiene " + edad + " años";
+    }
+}
